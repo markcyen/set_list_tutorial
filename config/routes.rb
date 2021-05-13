71bc2a7fde43d19e26f8124d4c90e5ed7e17ba7f
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-12 21:13:48 -0600

    Adds artist songs index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/songs', to: 'songs#index'
+  get '/songs/:id', to: 'songs#show' # :id is dynamic, a placeholder, similar to variable
+
+  get '/artists/:artist_id/songs', to: 'artist_songs#index'
+  # get '/songs/show_me/:foo', to: 'songs#custom'
+  # ':' is a domain specific language (DSL), dynamic value, not ruby symbol
+  # get /show/:me/the/songs', to: 'songs#display_them'
+  # ie, localhost:3000/show/jamison/the/songs
+
 end
